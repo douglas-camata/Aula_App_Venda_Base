4142b294598760debe573f1af6f9f2c10ebaddc7
Douglas Camata <douglas.camata@gmail.com>
2024-05-12 17:30:58 -0300

    aula 13_05_24

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import { View, Text} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
+    const [nome, setNome] = useState('');
+    const [tipoAcesso, setTipoAcesso] = useState('');
+
+    const verificarLoginRealizado = async () => {
+        let dadosLogado = await AsyncStorage.getItem('DadosUsuárioLogado');
+        if (dadosLogado) {
+            dadosLogado = JSON.parse(dadosLogado);
+            console.log(dadosLogado);
+            setNome(dadosLogado.nome)
+            setTipoAcesso(dadosLogado.tipoAcesso)
+        }
+    }
+
+    useEffect(() => {
+        verificarLoginRealizado();
+    }, []);
+
+    const botaoLogout = async () => {
+        await AsyncStorage.removeItem('DadosUsuárioLogado')
+        navigation.navigate('BemVindo')
+    }
+
     return (
         <View>
             <Text>Tela de Home</Text>
+            <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', padding:20}}>
+                <View>
+                    <Text style={{ fontSize: 16 }}>Nome: </Text>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{nome}</Text>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{tipoAcesso}</Text>
+                </View>
+                <MaterialIcons name='logout' size={25} onPress={botaoLogout} />
+            </View>
+
         </View>
     )
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,39 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native'
 import * as Animar from 'react-native-animatable'
 import { Picker } from '@react-native-picker/picker';
 import { corBranco, meusEstilos } from "../../style/MeusEstilos"
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Checkbox } from 'expo-checkbox';
 
 const Login = ({ navigation }) => {
     const [tipoAcesso, setTipoAcesso] = useState('vendedor')
     const [usuario, setUsuario] = useState('')
     const [senha, setSenha] = useState('')
     const [mensagemLogin, setMensagemLogin] = useState('')
+    const [permanecerConectado, setPermanecerConectado] = useState(false);
+
+    const verificarLoginRealizado = async () => {
+        let dadosLogado = await AsyncStorage.getItem('DadosUsuárioLogado');
+        if (dadosLogado) {
+            dadosLogado = JSON.parse(dadosLogado);
+            if (dadosLogado.permanecerConectado) 
+                navigation.navigate('MenuNavegacao', { tipo_usuario: dadosLogado.tipoAcesso })
+        }
+    }
+
+    useEffect(() => {
+        verificarLoginRealizado();
+    }, []);
 
     const botaoEntrar = async () => {
         try {
             // URL do endpoint da API de Login
             const resposta = await fetch('http://192.168.0.237:5000/usuarios/login', {
                 method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body : JSON.stringify({
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
                     usuario: usuario,
                     senha: senha,
                     tipo_usuario: tipoAcesso
@@ -28,10 +45,18 @@ const Login = ({ navigation }) => {
             }
 
             const json = await resposta.json()
-            if (json.length == 0) 
+            if (json.length == 0)
                 setMensagemLogin('Usuário e/ou senha não encontrado')
-            else
-                navigation.navigate('MenuNavegacao', {tipo_usuario: tipoAcesso})
+            else {
+                await AsyncStorage.setItem('DadosUsuárioLogado', JSON.stringify({
+                    usuario: usuario,
+                    nome: json[0].nome,
+                    tipoAcesso: tipoAcesso,
+                    permanecerConectado: permanecerConectado,
+                    id_usuario : json[0].id_usuario
+                }));
+                navigation.navigate('MenuNavegacao', { tipo_usuario: tipoAcesso })
+            }
 
         } catch (error) {
             console.error('Erro ao realizar o login:', error)
@@ -42,8 +67,8 @@ const Login = ({ navigation }) => {
         <View style={meusEstilos.conteudoHeader}>
             <Animar.View animation='fadeInLeft' delay={500} style={styles.header} >
                 <Image source={require('../../assets/logo.png')}
-                        style={styles.logo}
-                        resizeMode="contain" />
+                    style={styles.logo}
+                    resizeMode="contain" />
                 <Text style={styles.headerText}>Bem-vindo(a) </Text>
             </Animar.View>
             <Animar.View animation={'fadeInUp'} style={meusEstilos.conteudoCorpo}>
@@ -59,18 +84,24 @@ const Login = ({ navigation }) => {
                     </Picker>
                 </View>
                 <Text style={styles.label}> Email:</Text>
-                <TextInput 
-                    placeholder="Digite um email..." 
-                    style={styles.inputLogin} 
+                <TextInput
+                    placeholder="Digite um email..."
+                    style={styles.inputLogin}
                     value={usuario}
-                    onChangeText={setUsuario}/>
+                    onChangeText={setUsuario} />
                 <Text style={styles.label}> Senha:</Text>
-                <TextInput 
-                    placeholder="Digite sua senha" 
-                    style={styles.inputLogin} 
-                    secureTextEntry={true} 
+                <TextInput
+                    placeholder="Digite sua senha"
+                    style={styles.inputLogin}
+                    secureTextEntry={true}
                     value={senha}
-                    onChangeText={setSenha}/>
+                    onChangeText={setSenha} />
+
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Checkbox value={permanecerConectado} onValueChange={setPermanecerConectado} />
+                    <Text>Permanecer conectado</Text>
+                </View>
+
                 <TouchableOpacity style={meusEstilos.botao}
                     onPress={botaoEntrar}>
                     <Text style={meusEstilos.textoBotao}> Acessar </Text>
@@ -93,9 +124,9 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: corBranco
     },
-    logo : {
-        width: 30, 
-        height: 30, 
+    logo: {
+        width: 30,
+        height: 30,
         marginRight: 20
     },
     picker: {
